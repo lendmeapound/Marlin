63d427a56ec35d3b827d0974296d0b5605569ca3
Andy Chan <andy.chan@digital.landregistry.gov.uk>
2021-03-30 00:53:46 +0100

    Adding microswiss DD Changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -800,7 +800,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 136.4 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1237,8 +1237,8 @@
 #define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -5
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS 249
 #define Y_MAX_POS Y_BED_SIZE
